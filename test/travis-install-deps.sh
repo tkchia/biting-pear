4d4c0eb200587b2cd5e2a2ec0431c42aee42957f
Tee-Kiah Chia <hapax.mmxiii@icloud.com>
2016-03-27 00:19:49 +0800

    Trying out Travis CI.  (9)
    .travis.yml :
    - removed debugging stuff
    - added armv6-linux-gnueabi target
    
    test/travis-install-deps.sh :
    - fixed really bad typo

--- test/travis-install-deps.sh
+++ test/travis-install-deps.sh
@@ -2,9 +2,9 @@
 
 set -e -v -- libboost-dev binutils-multiarch-dev
 case "$TARGET" in
-    i[3456]86)
+    i[3456]86-*)
 	set -- ${1+"$@"} g++-multilib;;
-    arm*)
+    armv[45678]* | arm-*)
 	set -- ${1+"$@"} g++-arm-linux-gnueabi qemu-user;;
 esac
 exec sudo apt-get install -y ${1+"$@"}
