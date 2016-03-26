8bbcb1b6f79d58d62820ce37be7980d295f3c3eb
Tee-Kiah Chia <hapax.mmxiii@icloud.com>
2016-03-27 00:13:35 +0800

    Trying out Travis CI.  (8)
    test/travis-install-deps.sh :
    - rewrote dependency list for i386 compilation...

--- test/travis-install-deps.sh
+++ test/travis-install-deps.sh
@@ -3,7 +3,7 @@
 set -e -v -- libboost-dev binutils-multiarch-dev
 case "$TARGET" in
     i[3456]86)
-	set -- ${1+"$@"} lib32stdc++6 lib32stdc++-4.8-dev qemu-user;;
+	set -- ${1+"$@"} g++-multilib;;
     arm*)
 	set -- ${1+"$@"} g++-arm-linux-gnueabi qemu-user;;
 esac
