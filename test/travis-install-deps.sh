038baa1f542e07ecb4f7ec3b13f36319ec2eae37
Tee-Kiah Chia <hapax.mmxiii@icloud.com>
2016-03-26 23:50:25 +0800

    Trying out Travis CI.  (5)
    test/travis-install-deps.sh :
    - now installs more dependencies...

--- test/travis-install-deps.sh
+++ test/travis-install-deps.sh
@@ -2,7 +2,9 @@
 
 set -e -v -- libboost-dev binutils-multiarch-dev
 case "$TARGET" in
+    i[3456]86)
+	set -- ${1+"$@"} lib32stdc++-4.8-dev;;
     arm*)
-	set -- ${1+"$@"} g++-arm-linux-gnueabi;;
+	set -- ${1+"$@"} g++-arm-linux-gnueabi qemu-user;;
 esac
 exec sudo apt-get install -y ${1+"$@"}
