6f5ca0930124279339a501f4e199c5983dd7c84f
Tee-Kiah Chia <hapax.mmxiii@icloud.com>
2016-03-27 16:25:22 +0800

    aarch64-linux-gnu test target.  (3)
    test/travis-install-deps.sh :
    - now also runs `sudo apt-get update -y' before installing
      extra packages

--- test/travis-install-deps.sh
+++ test/travis-install-deps.sh
@@ -6,7 +6,6 @@ case "$TARGET" in
 	set -- ${1+"$@"} g++-multilib;;
     armv[45678]* | arm-*)
 	set -- ${1+"$@"} g++-arm-linux-gnueabi qemu-user;;
-    aarch64-*)
-	set -- ${1+"$@"} g++-aarch64-linux-gnu qemu-user;;
 esac
+sudo apt-get update -y
 exec sudo apt-get install -y ${1+"$@"}
