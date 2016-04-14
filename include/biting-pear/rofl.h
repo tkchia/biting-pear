3d221e072379b6f1ce1cd54756c20ce44e2eadd1
Tee-Kiah Chia <hapax.mmxiii@icloud.com>
2016-04-15 00:08:38 +0800

    Memory wiping on ARM.
    include/biting-pear/rofl.h :
    - specific implementations of biting_pear::rofl<...> for ARM
    - slight tweak of biting_pear::rofl<...> for x86-32 and
      x86-64

--- include/biting-pear/rofl.h
+++ include/biting-pear/rofl.h
@@ -22,7 +22,6 @@ class rofl
 #if defined __i386__
 		void *edi, *ecx;
 		__asm __volatile(
-			"addl $3, %%edi; "
 			"andl $-4, %%edi; "
 			"call 1f; "
 			"1: "
@@ -33,11 +32,11 @@ class rofl
 			".balign 4; "
 			"2: "
 			"rep; stosl"
-		    : "=D" (edi), "=c" (ecx) : "0" (s) : "memory", "cc");
+		    : "=D" (edi), "=c" (ecx)
+		    : "0" ((char *)s + 3) : "memory", "cc");
 #elif defined __amd64__
 		void *rdi, *rcx;
 		__asm __volatile(
-			"addq $7, %%rdi; "
 			"andq $-8, %%rdi; "
 			"leaq 2f(%%rip), %%rcx; "
 			"subq %%rdi, %%rcx; "
@@ -46,13 +45,44 @@ class rofl
 			"2: "
 			"rep; stosq; "		// .. 4 bytes
 			"stosl"			// .:
-		    : "=D" (rdi), "=c" (rcx) : "0" (s) : "memory", "cc");
+		    : "=D" (rdi), "=c" (rcx)
+		    : "0" ((char *)s + 7) : "memory", "cc");
+#elif defined __arm__
+		void *x, *y, *foo;
+		/*
+		 * Should work under ARM, Thumb-16, and Thumb-32 modes.  The
+		 * difference between "divided" and "unified" Thumb-16
+		 * syntax is a pain to handle...
+		 */
+#   if defined __thumb__ && !defined __thumb2__
+#	define biting_pear_T16_INSN(insn, d, n) insn " " d ", " n "; "
+#   else
+#	define biting_pear_T16_INSN(insn, d, n) insn "s " d ", " d ", " n "; "
+#   endif
+		biting_pear::kthxbai<NewState, unsigned, Flags, Levels>
+		    three(3);
+		__asm __volatile(
+			biting_pear_T16_INSN("bic", "%0", "%4")
+			"adr %1, 2f; "
+			biting_pear_T16_INSN("sub", "%0", "%1")
+			"beq 3f; "
+			".balign 4; "
+			"2: "
+			"str %2, [%1, %0]; "
+			biting_pear_T16_INSN("add", "%0", "#4")
+			"bne 2b; "
+			"3: "
+		    : "=l" (x), "=l" (foo), "=l" (y)
+		    : "0" ((char *)s + 3), "l" ((unsigned)three)
+		    : "memory", "cc");
+		__builtin___clear_cache(s, foo);
+#   undef biting_pear_T16_INSN
 #else
 		biting_pear::kthxbai<NewState,
 		    biting_pear_decltype(&std::memset),
 		    Flags, Levels> f(std::memset);
 		/* A rather slippery method.  But it works.  So far. */
-		f(s, 0, (char *)&&foo - (char *)s); 
+		f(s, 0, (char *)&&foo - (char *)s);
 	    foo:
 		__builtin___clear_cache(s, &&foo);
 #endif
