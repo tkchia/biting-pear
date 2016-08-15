5b9d63326cbf801ee81f5dd0e8ddb58010da0674
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2016-08-16 00:11:35 +0800

    Scrambling of (some) data sections, finally.  (3)
    share/innocent-pear/doge-98.cc :
    - separated out the call to rofl<...>::memset(...) from
      everything else

--- share/innocent-pear/doge-98.cc
+++ share/innocent-pear/doge-98.cc
@@ -14,22 +14,34 @@ extern unsigned char our_text_end[] __asm("_.innocent_pear.text.end");
 extern unsigned char our_rodata_start[] __asm("_.innocent_pear.rodata.start");
 extern unsigned char our_rodata_end[] __asm("_.innocent_pear.rodata.end");
 
-__attribute__((
 #ifdef innocent_pear_HAVE_CTOR_PRIORITY
-    constructor(65535),
+#   define innocent_pear_CTOR_L	constructor(65535)
 #else
-    constructor,
+#   define innocent_pear_CTOR_L	constructor
 #endif
-    section(".text.startup.zzzzzzzzzzzzzzzzzzzzzzzz"),
-    optimize("no-reorder-blocks")))
-static void scramble()
+#define innocent_pear_DOGE_L \
+	__attribute__((innocent_pear_CTOR_L, \
+	    section(".text.startup.zzzzzzzzzzzzzzzzzzzzzzzz"))) \
+	static void
+#define innocent_pear_DOGE_MEMSET_L \
+	__attribute__((innocent_pear_CTOR_L, \
+	    section(".text.startup.zzzzzzzzzzzzzzzzzzzzzzzz"), \
+	    optimize("no-reorder-blocks"))) \
+	static void
+
+innocent_pear_DOGE_MEMSET_L scramble_98_1()
+{
+	using innocent_pear::ops::allow_debugger_unsafes;
+	innocent_pear::rofl?<allow_debugger_unsafes>::memset
+	    (startup_text_start);
+}
+
+innocent_pear_DOGE_L scramble_98_2()
 {
 	using innocent_pear::ops::allow_debugger_unsafes;
 	uintptr_t pg_sz = (uintptr_t)getpagesize();
 	uintptr_t prot_start = (uintptr_t)unlikely_text_start & -pg_sz;
 	uintptr_t prot_end = ((uintptr_t)our_text_end + pg_sz - 1) & -pg_sz;
-	innocent_pear::rofl?<allow_debugger_unsafes>::memset
-	    (startup_text_start);
 	innocent_pear::kthxbai?<unsigned, allow_debugger_unsafes>
 	    prot(PROT_READ | PROT_EXEC);
 	innocent_pear::rofl?<allow_debugger_unsafes>::mprotect
