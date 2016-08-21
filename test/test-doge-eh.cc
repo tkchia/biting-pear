9ab9abb7e87e013b6315f3a1ebb4e7fbf050741a
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2016-08-22 00:10:06 +0800

    "The moving cursor writes, and having written, blinks on."  (4)
    test/test-doge-eh.cc :
    - tweaked code formatting

--- test/test-doge-eh.cc
+++ test/test-doge-eh.cc
@@ -11,14 +11,14 @@ void foo()
 int main()
 {
 	try {
-		 std::cout << "try\n";
-		 foo();
+		std::cout << "try\n";
+		foo();
 	} catch (int i) {
-		 std::cout << "catch\n";
-		 return 0;
+		std::cout << "catch\n";
+		return 0;
 	} catch (unsigned i) {
-		 std::cout << "wrong catch...\n";
-		 return 1;
+		std::cout << "wrong catch...\n";
+		return 1;
 	}
 	std::cout << "oops\n";
 	return 2;
