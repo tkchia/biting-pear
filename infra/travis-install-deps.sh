a0c003ad5cb8848912322e75c37a71e7dfccaa2b
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2016-10-06 00:40:27 +0800

    Now with a C (i.e. non-C++) compiler front-end; untested.  (2)
    .travis.yml :
    - now sets $CC_FOR_TARGET appropriately
    
    infra/travis-install-deps.sh :
    - now also installs target-side C compiler for tests
    
    .gitignore :
    - added innocent-pear-cc

--- infra/travis-install-deps.sh
+++ infra/travis-install-deps.sh
@@ -4,27 +4,30 @@ set -e -v -- flex binutils-multiarch-dev xz-utils
 case "$TARGET" in
     i[3456]86-* | pentium[^4]* | k6-* | athlon-[^f]* | winchip* | \
     c3-* | geode-*)
-	if test g++-5 = "$CXX_FOR_TARGET" && ! g++-5 --version; then
+	if (test g++-5 = "$CXX_FOR_TARGET" && ! g++-5 --version) || \
+	   (test gcc-5 = "$CC_FOR_TARGET" && ! gcc-5 --version); then
 		#
-		# This is a hack to get some testing done with g++ 5 under
-		# Travis CI's Ubuntu Trusty VM.  Note that installing g++ 5
-		# on a Trusty box may also touch the libraries used by the
-		# g++ 4.x setup.
+		# This is a hack to get some testing done with g++ (and gcc)
+		# 5 under Travis CI's Ubuntu Trusty VM.  Note that
+		# installing g++ 5 on a Trusty box may also touch the
+		# libraries used by the g++ 4.x setup.
 		#
 		# When Travis gets an Ubuntu Xenial VM, this can probably
 		# go, but until then...
 		#
 		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 		sudo apt-get update -y
-		set -- ${1+"$@"} g++-5-multilib
+		set -- ${1+"$@"} g++-5-multilib gcc-5-multilib
 	else
-		set -- ${1+"$@"} g++-multilib
-	fi;;
+		set -- ${1+"$@"} g++-multilib gcc-multilib
+	fi
     armv[45678]*hf | arm-*hf)
-	set -- ${1+"$@"} g++-arm-linux-gnueabihf qemu-user;;
+	set -- ${1+"$@"} g++-arm-linux-gnueabihf gcc-arm-linux-gnueabihf \
+	    qemu-user;;
     armv[45678]* | arm-*)
-	set -- ${1+"$@"} g++-arm-linux-gnueabi qemu-user;;
+	set -- ${1+"$@"} g++-arm-linux-gnueabi gcc-arm-linux-gnueabi \
+	    qemu-user;;
 esac
-if test clang++ = "$CXX_FOR_TARGET"
+if test clang++ = "$CXX_FOR_TARGET" -o clang = "$CC_FOR_TARGET"
 	then set -- ${1+"$@"} clang; fi
 exec sudo apt-get install -y ${1+"$@"}
