d83379b805c2c3a5bd387f41da23b639b40d33be
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2017-05-04 00:20:52 +0800

    More ia16-elf hacks.
    test/test-orly-3.cc :
    - hacks conditional on ia16-elf:
      - removed argument parsing
      - now avoids using PRIxLEAST64

--- test/test-orly-3.cc
+++ test/test-orly-3.cc
@@ -13,12 +13,20 @@ int main(int argc, char **argv)
 	innocent_pear::kthxbai?<innocent_pear_decltype(&std::printf),
 	    allow_for_startup, 1u> pf(std::printf);
 	uint_least64_t c0, z0, w0;
-	if (argc < 2)
-		c0 = 0x67452301efcdab89ull;
-	else
+	c0 = 0x67452301efcdab89ull;
+#ifndef __ia16__
+	if (argc > 1)
 		c0 = std::strtoul(argv[1], 0, 0);
+#endif
 	z0 = f0(c0);
 	w0 = g0(z0);
+#ifndef __ia16__
 	pf("%#" PRIxLEAST64 "\n", w0);
+#else
+	/* PRIxLEAST64 does not work correctly under ia16-elf. */
+	using innocent_pear::impl::uint_least32_t;
+	pf("0x%08" PRIxLEAST32 "%08" PRIxLEAST32 "\n",
+	    (uint_least32_t)(w0 >> 32), (uint_least32_t)w0);
+#endif
 	return 0;
 }
