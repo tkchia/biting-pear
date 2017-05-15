5631a786fbac945a08c7ae2f3eed2dce15492f0a
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2017-05-16 00:56:52 +0800

    A bit more ia16-elf hacking.
    share/innocent-pear/doge-40.cc :
    - hacks for ia16-elf:
      - removed unneeded #include <sys/mman.h>
      - now uses innocent_pear_BIG_BAD
    
    infra/lolwutconf.tar.xz :
    - updated

--- share/innocent-pear/doge-40.cc
+++ share/innocent-pear/doge-40.cc
@@ -3,7 +3,6 @@
 #include <cstdlib>
 #include <cstring>
 #include <unistd.h>
-#include <sys/mman.h>
 #include <innocent-pear/dawg.h>
 #include <innocent-pear/kthxbai.h>
 #include <innocent-pear/orly.h>
@@ -88,7 +87,8 @@ innocent_pear_DOGE unscramble_40_1()
 	innocent_pear::kthxbai?<unsigned char *, flags> nxe(next_end);
 	innocent_pear_CHAFF(flags);
 	innocent_pear::orly<innocent_pear_DOGE_STATE_9,
-	    unsigned char, false, true, flags>().wut(nxs, nxe);
+	    unsigned char, false, innocent_pear_BIG_BAD, flags>().
+	    wut(nxs, nxe);
 	innocent_pear_CHAFF(flags);
 	innocent_pear::kthxbai?<unsigned char *, flags, 0u>
 	    dcs(dogecoin_start);
