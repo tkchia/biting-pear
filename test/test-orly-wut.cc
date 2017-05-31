b18b945192875c121c5dfe791fbe4ac14d5147c2
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2017-05-31 23:28:32 +0800

    orly<...>::wut(...) was broken on ia16-elf.  (2)
    test/test-orly-wut.cc :
    - fixed improper use of kthxbai<...>(, 3)

--- test/test-orly-wut.cc
+++ test/test-orly-wut.cc
@@ -8,9 +8,8 @@ char msg[] = "SCIENTIA EST LVX LVCIS\n";
 int main()
 {
 	innocent_pear::kthxbai?<unsigned char *> p((unsigned char *)msg, 3);
-	innocent_pear::kthxbai?<unsigned char *> q((unsigned char *)msg
-	    + sizeof msg, 3);
-	innocent_pear::orly<STATE, unsigned char, false, true>().wut(p, q);
+	innocent_pear::orly<STATE, unsigned char, false, true>().wut(p,
+	    p + sizeof msg);
 	std::fputs(msg, stdout);
 	return 0;
 }
